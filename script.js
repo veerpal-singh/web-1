436ae303e264b28c71b3761810ee0799654ce117
veerpal-singh <veer.veeru68@gmail.com>
2024-04-28 06:21:11 +0100

    add new file

---
+++ script.js
@@ -0,0 +1,18 @@
+// Activate Carousel
+$(".all-card").carousel();
+
+// Enable Carousel Indicators
+$(".item").click(function(){
+  $(".all-card").carousel(1);
+});
+
+// Enable Carousel Controls
+$(".left").click(function(){
+  $(".all-card").carousel("prev");
+});
+
+$(".all-card").carousel();
+
+$(".item").click(function()){
+    
+}
